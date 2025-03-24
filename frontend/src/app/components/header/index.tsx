899b8e2791a825ba046046e3b3d058ebf8dc5537
dev-akash-sarker <akashsarker210@gmail.com>
2025-03-25 00:11:57 +0600

    home banner added

--- frontend/src/app/components/header/index.tsx
+++ frontend/src/app/components/header/index.tsx
@@ -1,7 +1,64 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 const Header: FC = () => {
-  return <h1>Hekki</h1>;
+  interface Menu {
+    id?: number;
+    pagename: string;
+    pagelink: string;
+  }
+
+  const menu: Menu[] = [
+    {
+      id: 0,
+      pagename: "home",
+      pagelink: "/",
+    },
+    {
+      id: 1,
+      pagename: "shop",
+      pagelink: "/shop",
+    },
+    {
+      id: 2,
+      pagename: "about",
+      pagelink: "/about",
+    },
+    {
+      id: 3,
+      pagename: "contact",
+      pagelink: "/contact",
+    },
+    {
+      id: 4,
+      pagename: "journal",
+      pagelink: "/journal",
+    },
+  ];
+  return (
+    <>
+      <div className="container mx-auto">
+        <nav className=" flex justify-between py-8 md:px-0 lg:px-40">
+          <div>
+            <Link href="/">
+              <Image src="/logo/logo.png" width={65} height={15} alt="orebi" />
+            </Link>
+          </div>
+          <ul className=" flex justify-start items-center gap-10">
+            {menu.map((item) => (
+              <li key={item.id}>
+                <Link href={item.pagelink} className="capitalize">
+                  {item.pagename}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Header;
---
+++ frontend/src/app/components/home/banner/Banner.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Slider, { Settings } from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function Banner() {
+  const [dotActive, setDotActive] = useState<number>(0);
+
+  const settings: Settings = {
+    dots: true,
+    beforeChange: (_prev, next) => setDotActive(next),
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    fade: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "60%",
+          transform: "translateY(-50%)",
+          left: "8%",
+          width: "50%",
+        }}
+      >
+        <ul style={{ margin: 0 }}>{dots}</ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: "30px",
+                color: "#262626",
+                borderRight: "3px black solid",
+                padding: "10px 0",
+              }
+            : {
+                width: "30px",
+                color: "transparent",
+                borderRight: "3px white solid",
+                padding: "10px 0",
+              }
+        }
+      >
+        0{i + 1}
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          dots: true,
+          appendDots: (dots) => (
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                left: "2%",
+              }}
+            >
+              <ul style={{ margin: "0px" }}>{dots}</ul>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={
+                i === dotActive
+                  ? {
+                      width: "30px",
+                      color: "#262626",
+                      borderRight: "3px black solid",
+                      fontSize: "12px",
+                    }
+                  : {
+                      width: "30px",
+                      color: "transparent",
+                      borderRight: "3px white solid",
+                      fontSize: "12px",
+                    }
+              }
+            >
+              0{i + 1}
+            </div>
+          ),
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="bg-[#F5F7F9]">
+      <Slider className="sliderbanner" {...settings}>
+        {["bannerone.png", "bannertwo.png", "bannerone.png"].map(
+          (banner, index) => (
+            <Link className=" w-full" href="#" key={index}>
+              <div className="w-full h-[597px] bg-[#F5F7F9]">
+                <Image
+                  src={`/banner/${banner}`}
+                  fill
+                  className="w-auto h-auto"
+                  alt={banner.replace(".png", "")}
+                />
+              </div>
+            </Link>
+          )
+        )}
+      </Slider>
+    </div>
+  );
+}
